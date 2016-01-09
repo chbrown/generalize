110faa4bdac346da6cfdd28b066d5068b2f1c914
Christopher Brown <io@henrian.com>
2016-01-08 19:59:40 -0600

    Fix generalize to return actual valid JSON schemas; improve test coverage with null and undefined examples

--- index.ts
+++ index.ts
@@ -1,3 +1,29 @@
+/*!
+Many documentation snippets below are taken verbatim from the JSON Schema spec:
+http://json-schema.org/documentation.html
+*/
+
+// not sure if this const of primitives is useful.
+/**
+JSON Schema defines seven primitive types for JSON values:
+*/
+const primitives = {
+  /** A JSON array. */
+  'array': 'array',
+  /** A JSON boolean. */
+  'boolean': 'boolean',
+  /** A JSON number without a fraction or exponent part. */
+  'integer': 'integer',
+  /** Any JSON number. Number includes integer. */
+  'number': 'number',
+  /** The JSON null value. */
+  'null': 'null',
+  /** A JSON object. */
+  'object': 'object',
+  /** A JSON string. */
+  'string': 'string',
+};
+
 export interface Schema {
   /**
   type must be one of the seven primitive JSON schema types:
@@ -10,24 +36,100 @@ export interface Schema {
   - string
 
   number is a superset of integer.
-  */
-  type: string;
-  [index: string]: any;
-}
 
-export interface ObjectSchema extends Schema {
-  properties: {[index: string]: Schema[]};
+  Or if is undefined, the schema is considered 'empty' and will match anything.
+  */
+  type?: string | string[];
+  [index: string]: any;
 }
 
 /**
 JSON Schema calls both tuple types and list types 'array'.
 See http://json-schema.org/latest/json-schema-validation.html#anchor128
 
-ArraySchema implements the list sense, i.e., when `items` is a schema, not an
-array.
+The list type interpretation is indicated when `items` is a Schema.
+The tuple type interpretation is indicated when `items` is an array of Schemas.
 */
 export interface ArraySchema extends Schema {
-  items: Schema[];
+  // type: "array",
+  /**
+  The value of "additionalItems" MUST be either a boolean or an object. If it
+  is an object, this object MUST be a valid JSON Schema.
+  */
+  additionalItems?: boolean | Schema;
+  /**
+  The value of "items" MUST be either an object or an array. If it is an object,
+  this object MUST be a valid JSON Schema. If it is an array, items of this
+  array MUST be objects, and each of these objects MUST be a valid JSON Schema.
+
+  * If "items" is a schema, then the child instances must be valid against this
+    schema, regardless of their index, and regardless of the value of
+    "additionalItems".
+  * If "items" is an array, the schema depends on the index:
+    - if the index is less than, or equal to, the size of "items", the child
+      instance must be valid against the corresponding schema in the "items" array;
+    - otherwise, it must be valid against the schema defined by "additionalItems".
+  */
+  items: Schema | Schema[];
+}
+
+export interface ObjectSchema extends Schema {
+  // type: "object",
+  /**
+  The value of this keyword MUST be an integer. This integer MUST be greater
+  than, or equal to, 0. An object instance is valid against "maxProperties" if
+  its number of properties is less than, or equal to, the value of this keyword.
+  */
+  maxProperties?: number;
+  /**
+  The value of this keyword MUST be an integer. This integer MUST be greater
+  than, or equal to, 0. An object instance is valid against "minProperties" if
+  its number of properties is greater than, or equal to, the value of this
+  keyword. If this keyword is not present, it may be considered present with a
+  value of 0.
+  */
+  minProperties?: number;
+  /**
+  The value of this keyword MUST be an array. This array MUST have at least
+  one element. Elements of this array MUST be strings, and MUST be unique. An
+  object instance is valid against this keyword if its property set contains all
+  elements in this keyword's array value.
+  */
+  required?: string[];
+  /**
+  The value of "additionalProperties" MUST be a boolean or an object. If it
+  is an object, it MUST also be a valid JSON Schema. If it is absent, it may be
+  considered present with an empty schema as a value. Successful validation of
+  an object instance against the three keywords, "additionalProperties",
+  "properties", and "patternProperties", depends on the value of
+  "additionalProperties":
+  1. if its value is boolean true or a schema, validation succeeds;
+  2. if its value is boolean false, validation of the instance depends on the
+    property set of "properties" and "patternProperties":
+    a. Collect the following sets:
+       s: The property set of the instance to validate.
+       p: The property set from "properties".
+       pp: The property set from "patternProperties".
+    b. Remove from "s" all elements of "p", if any;
+    c. For each regex in "pp", remove all elements of "s" which this regex
+       matches.
+    Validation of the instance succeeds if set "s" is now empty.
+  */
+  additionalProperties?: boolean | Schema;
+  /**
+  The value of "properties" MUST be an object. Each value of this object MUST be
+  an object, and each object MUST be a valid JSON Schema. If it is absent, it
+  can be considered present with an empty object as a value.
+  */
+  properties?: {[index: string]: Schema};
+  /**
+  The value of "patternProperties" MUST be an object. Each property name of this
+  object SHOULD be a valid regular expression, according to the ECMA 262 regular
+  expression dialect. Each property value of this object MUST be an object, and
+  each object MUST be a valid JSON Schema. If it is absent, it can be considered
+  present with an empty object as a value.
+  */
+  patternProperties?: {[index: string]: Schema};
 }
 
 /**
@@ -35,7 +137,7 @@ Returns one of the seven primitive JSON schema types, or 'undefined':
   array, boolean, integer, number, null, object, string, undefined
 (Except it does not actually ever return 'integer'.)
 */
-function valueType(value: any): string {
+function primitiveType(value: any): string {
   if (value === undefined) {
     return 'undefined';
   }
@@ -49,75 +151,91 @@ function valueType(value: any): string {
   return typeof value;
 }
 
+function typeMatches(instanceType: string, schemaType: string | string[]): boolean {
+  if (schemaType === undefined) {
+    return false;
+  }
+  else if (Array.isArray(schemaType)) {
+    return schemaType.indexOf(instanceType) > -1;
+  }
+  else {
+    return instanceType === schemaType;
+  }
+}
 /**
-Create an empty schema with an empty collection value, depending on the value
-of the given type.
+Assumes that schemaType does not already equal or include instanceType.
 */
-function createEmptySchema(type: string): Schema {
-  if (type == 'object') {
-    return {type, properties: {}};
+function addType(instanceType: string, schemaType: string | string[]): string | string[] {
+  if (schemaType === undefined) {
+    // if we are adding a type to an empty schema, its type will be undefined
+    return instanceType;
   }
-  else if (type == 'array') {
-    return {type, items: []};
+  else if (Array.isArray(schemaType)) {
+    return schemaType.concat(instanceType);
   }
-  return {type};
+  else {
+    return [schemaType, instanceType];
+  }
 }
 
 /**
-Return the schema in schemas such that schema.type == `type`, or `undefined`
-if no schema matches.
+union takes a potentially insufficiently general schema, and an instance that
+the schema must be extended to include, and returns a new schema that matches
+everything the original schema did, as well as the new instance.
 */
-function find(schemas: Schema[], type: string) {
-  for (var i = 0, schema: Schema; (schema = schemas[i]); i++) {
-    if (schema.type == type) {
-      return schema;
-    }
-  }
-}
-
-export function union(schemas: Schema[], value: any): Schema[] {
-  var type = valueType(value);
+export function union(schema: Schema, instance: any): Schema {
+  const type = primitiveType(instance);
   if (type == 'undefined') {
     // TODO: handle this case. What should I do with an array of values like
     // `['laugh', 100, undefined]`? change all of the schemas to optional?
     // schemas.forEach(schema => schema.optional = true);
-    return schemas;
+    return schema;
   }
-
-  // find a pre-existing schema, if there is one
-  var schema = find(schemas, type);
-  if (schema === undefined) {
-    // if the schema is unset, there's no precedent, so it's easy
-    schema = createEmptySchema(type);
-    schemas.push(schema);
-  }
-
-  // merge into existing schema.
-  if (type == 'object') {
-    // only the nested types, object and array (below), require merging.
-    var object_schema = <ObjectSchema>schema;
-    for (var key in value) {
-      // should we actually do the `hasOwnProperty` check?
-      if (value.hasOwnProperty(key)) {
-        object_schema.properties[key] = union(object_schema.properties[key] || [], value[key]);
-      }
+  else if (type == 'object') {
+    // TODO: clone the schema instead of mutating it
+    let objectSchema = <ObjectSchema>schema;
+    if (!typeMatches(type, objectSchema.type)) {
+      // the schema does not currently handle objects, so we need to add the type
+      objectSchema.type = addType(type, objectSchema.type)
+      // and the appropriate field
+      objectSchema.properties = {};
     }
+    // now we can merge the object's fields
+    for (var key in instance) {
+      // should we do a `hasOwnProperty` check?
+      objectSchema.properties[key] = union(objectSchema.properties[key] || {}, instance[key]);
+    }
+    return objectSchema;
   }
   else if (type == 'array') {
-    // throw new Error('array merge not yet supported');
-    var array_schema = <ArraySchema>schema;
-    value.forEach(item => {
-      array_schema.items = union(array_schema.items, item);
+    // TODO: support tuple-type arrays?
+    // TODO: clone the existing schema
+    let arraySchema = <ArraySchema>schema;
+    if (!typeMatches(type, arraySchema.type)) {
+      // the schema does not currently handle arrays, so we need to add the type
+      arraySchema.type = addType(type, arraySchema.type)
+      // and the appropriate field
+      arraySchema.items = {};
+    }
+    // now we can merge the array's schema
+    instance.forEach(item => {
+      arraySchema.items = union(arraySchema.items, item);
     });
+    return arraySchema;
   }
   else {
-    // otherwise, nothing needs to be done.
+    if (!typeMatches(type, schema.type)) {
+      schema.type = addType(type, schema.type)
+    }
+    // only the nested types, object and array, require merging. otherwise, nothing needs to be done.
+    return schema;
   }
-  return schemas;
 }
 
-export function generalizeArray(values: any[]): Schema[] {
-  return values.reduce((accumulator_schemas, value) => {
-    return union(accumulator_schemas, value);
-  }, []);
+export function generalizeFrom(...instances: any[]): Schema {
+  return instances.reduce((schema, instance) => {
+    return union(schema, instance);
+  }, {});
 }
+
+export default generalizeFrom;
--- tests/basic.js
+++ tests/basic.js
@@ -1,127 +1,121 @@
 import assert from 'assert';
 import {describe, it} from 'mocha';
-import {generalizeArray} from '../';
-
-function assertEqualSchemas(actual, expected) {
-  // FIXME: this should test equality as sets
-  assert.deepEqual(actual, expected);
-}
+import {generalizeFrom} from '../';
 
 describe('basic object generalization', () => {
 
-  it('should collapse number types', () => {
-    var actual = generalizeArray([
+  // TODO: compare set values as sets, e.g., when type: string[]
+
+  it('should generalize homogenous number types', () => {
+    var actual = generalizeFrom(
       100,
       -90,
       45,
       3.14159,
-    ]);
-    assertEqualSchemas(actual, [{
-      type: 'number',
-    }]);
+    );
+    assert.deepEqual(actual, {type: 'number'});
   });
 
-  it('should collapse string types', () => {
-    var actual = generalizeArray([
+  it('should generalize homogenous string types', () => {
+    var actual = generalizeFrom(
       'I could',
       'not',
       'care',
       'less!',
-    ]);
-    assertEqualSchemas(actual, [{
-      type: 'string',
-    }]);
+    );
+    assert.deepEqual(actual, {type: 'string'});
   });
 
-  it('should collapse identical object types', () => {
-    var actual = generalizeArray([
-      {
-        name: 'Curious George',
-        age: 5,
-      },
-      {
-        name: 'Mark Bedelman',
-        age: 47,
-      },
-    ]);
-    assertEqualSchemas(actual, [{
+  it('should generalize nullable string types', () => {
+    var actual = generalizeFrom(
+      'Hello',
+      null,
+      'world!',
+      null,
+    );
+    assert.deepEqual(actual, {type: ['string', 'null']});
+  });
+
+  it('should generalize more than two distinct types', () => {
+    var actual = generalizeFrom(
+      null,
+      'Hello',
+      123,
+    );
+    assert.deepEqual(actual, {type: ['null', 'string', 'number']});
+  });
+
+  it('should ignore undefined instances', () => {
+    var actual = generalizeFrom(
+      1,
+      2,
+      undefined,
+      3,
+    );
+    assert.deepEqual(actual, {type: 'number'});
+  });
+
+  it('should generalize identical object types', () => {
+    var actual = generalizeFrom(
+      { name: 'Curious George', age: 5 },
+      { name: 'Mark Bedelman', age: 47 },
+    );
+    assert.deepEqual(actual, {
       type: 'object',
       properties: {
-        name: [{
-          type: 'string',
-        }],
-        age: [{
-          type: 'number',
-        }],
+        name: {type: 'string'},
+        age: {type: 'number'},
       },
-    }]);
+    });
   });
 
-  it('should build array from distinct object types', () => {
-    var actual = generalizeArray([
+  it('should build schema from distinct types', () => {
+    var actual = generalizeFrom(
       'incredible',
       800,
-    ]);
-    assertEqualSchemas(actual, [{
-      type: 'string',
-    }, {
-      type: 'number',
-    }]);
+    );
+    assert.deepEqual(actual, {
+      type: ['string', 'number'],
+    });
   });
 
-  it('should build array from objects with distinct property sets', () => {
-    var actual = generalizeArray([
-      {
-        name: 'Curious George',
-        age: 5,
-      },
-      {
-        breed: 'Schnauzer',
-        age: false,
-      },
-    ]);
-    assertEqualSchemas(actual, [{
+  it('should build schema from objects with distinct property sets', () => {
+    var actual = generalizeFrom(
+      { name: 'Curious George', age: 5 },
+      { breed: 'Schnauzer', age: false },
+    );
+    assert.deepEqual(actual, {
       type: 'object',
       properties: {
-        name: [{
-          type: 'string',
-        }],
-        breed: [{
-          type: 'string',
-        }],
-        age: [{
-          type: 'number',
-        }, {
-          type: 'boolean',
-        }],
+        name: {type: 'string'},
+        breed: {type: 'string'},
+        age: {
+          type: ['number', 'boolean'],
+        },
       },
-    }]);
+    });
   });
 
-  it('should build array schema from objects with identical object types but different lengths', () => {
-    var actual = generalizeArray([
+  it('should build schema from objects with identical object types but different lengths', () => {
+    var actual = generalizeFrom(
       [
-        { name: 'Curious George', email: 'cgeorge@example.org'},
+        { name: 'Curious George', email: 'cgeorge@example.org' },
       ],
       [
-        { name: 'Bran Kurtz', email: 'bkurtz@example.org'},
-        { name: 'Kev Bravado', email: 'kbravado@example.org'},
+        { name: 'Bran Kurtz', email: 'bkurtz@example.org' },
+        { name: 'Kev Bravado', email: 'kbravado@example.org' },
       ],
-    ]);
-    assertEqualSchemas(actual, [{
+    );
+    assert.deepEqual(actual, {
       type: 'array',
-      items: [{
+      items: {
         type: 'object',
         properties: {
-          name: [{
-            type: 'string',
-          }],
-          email: [{
-            type: 'string',
-          }],
+          name: {type: 'string'},
+          email: {type: 'string'},
         },
-      }],
-    }]);
+      },
+    });
   });
 
 });
